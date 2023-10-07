0f2d198ef270ec850c1600df292f38c80782e2f6
localtoast3000 <craigchick@outlook.com>
2023-10-07 21:10:28 +0200

    Template

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -8,8 +8,8 @@ import { AppNavigation } from '@/navigation/types';
 export default function Landing({ navigation }: AppNavigation<'landing'>) {
   return (
     <FullPageLayout scrollable={false}>
-      <H1 className='text-first w-full text-center mb-[20px]'>Open Chat</H1>
-      <ChatIcon className='h-[300px] mb-[30px]' />
+      <H1 className='text-first text-center'>Open Chat</H1>
+      <ChatIcon className='w-auto h-[300px]' />
       <Button
         label='Sign Up'
         onPress={() => navigation.push('sign-up')}
